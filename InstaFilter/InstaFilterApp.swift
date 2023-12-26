1ca3e96238c0ac50108335a665a078d7df183c8a
Henny2 <henrieke.baunack@web.de>
2023-12-25 18:37:23 -0800

    Initial Commit

---
+++ InstaFilter/InstaFilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaFilterApp.swift
+//  InstaFilter
+//
+//  Created by Henrieke Baunack on 12/25/23.
+//
+
+import SwiftUI
+
+@main
+struct InstaFilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
